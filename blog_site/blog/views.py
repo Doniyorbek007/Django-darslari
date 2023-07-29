71924758dcfa720d31e45660a0e337890c46ac46
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-07-29 12:43:26 +0500

    done

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Post
 
 # Create your views here.
@@ -7,3 +7,7 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'context'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
