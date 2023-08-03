6ad3a72d0ea25f6a06870443634c08f418945020
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-08-03 20:32:50 +0500

    done

--- yangiliklar_sayti/yangiliklar_sayti/settings.py
+++ yangiliklar_sayti/yangiliklar_sayti/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     # 3rd party apps
-    'crispy_forms',
+    "crispy_forms",
+    "crispy_bootstrap5"
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
