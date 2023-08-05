2b318b49248f7137325a299a40f66e7e8f75bd9f
Doniyorbek007 <qahramonovdoniyor05@gmail.com>
2023-08-05 15:11:57 +0500

    done

--- yangiliklar_sayti/yangiliklar_sayti/settings.py
+++ yangiliklar_sayti/yangiliklar_sayti/settings.py
@@ -132,7 +132,13 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = 587
+EMAIL_HOST_USER  = "postmaster@sandboxfd05b033cf7f47569a61f964c158f897.mailgun.org"
+EMAIL_HOST_PASSWORD = "bec872e2e12a82e956322b5dfb7a5056-4e034d9e-46c6cb4a"
+EMAIL_USE_TLS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
